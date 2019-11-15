08fa382f21f56d884b90f6b4949009164b1839f6
lmm <ethanlin1116@gmail.com>
2019-11-15 14:37:39 -0800

    statementMaker update

--- src/StatementMaker.java
+++ src/StatementMaker.java
@@ -1,58 +1,89 @@
 import java.lang.StringBuffer;
 
 public class StatementMaker {
-    public static String[] test1 = new String[]{"FIRST_NAME","LAST_NAME", "AGE", "ADDRESS"};
-    public static String[] test2 = new String[]{"Tom", "Whatever", "2", "123 Street"};
 
-
-    public StringBuffer asTuple(String[] lst, Boolean bracket) {
+    public String insert(String table_name, String[] fields_table, String[] values_table) {
+        if (fields_table.length != values_table.length) {
+            return "";
+        }
         StringBuffer result = new StringBuffer();
 
-        if (bracket) result.append("(");
-        for (int i = 0; i < lst.length - 1; i++) {
-            result.append(str(lst[i])+",");
+        INSERT_INTO(result, table_name, fields_table);
+        VALUE(result, values_table);
+
+        return result.append(";").toString();
+
     }
-        result.append(str(lst[lst.length - 1]));
-        if (bracket) result.append(")");
+
+
+    public String select(String[] select_colum, String from_statement, String where_statement) {
+        StringBuffer result = new StringBuffer();
+        SELECT(result, select_colum);
+        FROM(result, from_statement);
+        WHERE(result, where_statement);
+
+        return result.append(";").toString();
+    }
+
+    private void SELECT(StringBuffer target, String[] colum) {
+        if (colum.length > 0) target.append("SELECT " + strWrapper(colum, ",", false));
+        else target.append("SELECT *");
+    }
+
+    private void FROM(StringBuffer target, String Statement) {
+        target.append(" FROM " + Statement);
+    }
+
+    private void WHERE(StringBuffer target, String statement) {
+        if (!statement.equals("")) target.append(" WHERE " + statement);
+    }
+
+    private void INSERT_INTO(StringBuffer target, String table_name, String[] fields_table) {
+        target.append("INSERT INTO ");
+        target.append(table_name);
+        target.append(asTuple(fields_table, false));
+    }
+
+    private void VALUE(StringBuffer target, String[] values_table) {
+        target.append(" VALUE ");
+        target.append(asTuple(values_table, true));
+    }
+
+    //make life easier
+    public static String[] list(String... lst) {
+        return lst;
+    }
+
+    public static String str(String... x) {
+        return strWrapper(x, "", true).toString();
+    }
+
+    //a string wrapper, it will skip non-string String;
+    //isOn: auto wrapp string with "" if true
+    public static StringBuffer strWrapper(String[] lst, String spliter, Boolean isON) {
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < lst.length; i++) {
+            if (!isON || lst[i].matches("NULL|\\d|<|>|==|,")) result.append(lst[i]);
+            else result.append("\"" + lst[i] + "\"");
+            if (!spliter.equals("") && i < lst.length - 1) result.append(spliter);
+        }
         return result;
     }
 
-
-    public String insertStatement(String table_name, String[] fields_table, String[] values_tables) {
-        if (fields_table.length != values_tables.length) { return ""; }
-        return new StringBuffer("INSERT INTO " + table_name + asTuple(fields_table, true) + " VALUES" + asTuple(values_tables, true) + ";").toString();
-
+    public StringBuffer asTuple(String[] lst, boolean isON) {
+        StringBuffer result = new StringBuffer("(");
+        result.append(strWrapper(lst, ",", isON));
+        result.append(")");
+        return result;
     }
-
-    public String selectStatement(String[] select_colum, String from_statement, String where_statement) {
-        return new StringBuffer(SELECT(select_colum) + FROM(from_statement) + WHERE(where_statement) + ";").toString();
-    }
-    private String SELECT(String[] colum){
-        if(colum.length>0)return "SELECT "+asTuple(colum,false);
-        return "SELECT *";
-    }
-    private String FROM(String Statement){
-        return " FROM "+ Statement;
-    }
-    private String WHERE(String statement){
-        if(!statement.equals(""))return " WHERE "+statement;
-        return "";
-    }
-    //a string wrapper, it will skip non-string String;
-    public static String str(String x){
-        if(x.matches("-?\\d+(\\.\\d+)?")) return x;
-        if(x.matches("NULL")) return x;
-        return "\""+x+"\"";
-
-    }
-
-
 
     public static void main(String[] args) {
-        StatementMaker q = new StatementMaker();
-        String x = q.insertStatement("medical", test1, test2);
-        //x = q.selectStatement(new String[]{"A", "B"}, "CUSTOMER", str("test") +"==1");
+        StatementMaker s = new StatementMaker();
+
+        String x = s.insert("medical", list("FIRST_NAME", "LAST_NAME"), list("Tom", "Woods"));
+        String y = s.select(list("A", "B", "C", "D"), "CUSTOMER", str("test", "==", "1"));
         System.out.println(x);
+        System.out.println(y);
     }
 
 
