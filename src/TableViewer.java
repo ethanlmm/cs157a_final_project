65c42a0a89ec386884e1f53f4e26264842988fbc
Kurogisa <sarahmai434@gmail.com>
2019-11-27 18:35:16 -0800

    visit update

--- src/TableViewer.java
+++ src/TableViewer.java
@@ -8,10 +8,10 @@ import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
 
 public class TableViewer extends Viewer{
-	
+
 	private JTable table;
 	private TableListener listener;
-	
+
 	public TableViewer()
 	{
 		table = new JTable();
@@ -19,7 +19,7 @@ public class TableViewer extends Viewer{
 		table.setAutoCreateRowSorter(true);
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 	}
-	
+
 	public void setTable(DefaultTableModel model)
 	{
 		table.setModel(model);
@@ -27,61 +27,61 @@ public class TableViewer extends Viewer{
 		table.getModel().addTableModelListener(listener);
 		table.changeSelection(0, 0, false, false);
 	}
-	
+
 	public Object getTableRowContents(int col)
 	{
 		return table.getModel().getValueAt(table.getSelectedRow(), col);
 	}
-	
+
 	protected JTable getTable()
 	{
 		return table;
 	}
-	
+
 	public class TableListener implements TableModelListener
 	{
 		private JTable table;
 		private HashMap<Integer, ArrayList<Object>> changes;
-		
+
 		public TableListener()
 		{
 			changes = new HashMap<Integer, ArrayList<Object>>();
 		}
-		
+
 		public TableListener(JTable table)
 		{
 			this.table = table;
 			changes = new HashMap<Integer, ArrayList<Object>>();
 		}
-		
+
 		public void setTable(JTable table)
 		{
 			this.table = table;
 		}
-		
+
 		public void tableChanged(TableModelEvent e) {
 			if (e.getType() == TableModelEvent.UPDATE)
 			{
-//				
+//
 				int row = e.getFirstRow();
 				int col = e.getColumn();
-				
+
 				System.out.print("Change at ");
 				System.out.println(getTableContents(row, col));
 			}
 		}
-		
+
 		public void clearChanges()
 		{
 			changes.clear();
 		}
-		
+
 		public Object getTableContents(int row, int col)
 		{
 			return table.getModel().getValueAt(row, col);
 		}
-		
+
 	}
-	
+
 
 }
--- src/VisitViewer.java
+++ src/VisitViewer.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.event.*;
 
-public class VisitViewer
+public class VisitViewer extends TableViewer
 {
 	private final int TEXTFIELD_WIDTH = 20;
 
@@ -17,10 +17,30 @@ public class VisitViewer
 	private JSpinner yearSpinner = new JSpinner();
 
 	private JTextField protocolNum = new JTextField(TEXTFIELD_WIDTH);
+	private JSpinner Problem = new JSpinner();
+	private JSpinner category = new JSpinner();
+	private JTextField protocol = new JTextField(); //assigned random number
+	private JSpinner instrumental = new ();
+	private JCheckBox rem = new JCheckBox();
+	private JSpinner followUp = new JSpinner();
+	private JTextField addCComments = new JTextField(50);
+
+	private JButton saveButton = new JButton("Save");
+	private JButton cancelButton = new JButton("Cancel");
 
 	public VisitViewer()
 	{
 		JLabel dobLabel = new JLabel("Visit Date:", SwingConstants.RIGHT);
+		String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+		monthSpinner.setModel(new SpinnerListModel(months));
+		daySpinner.setModel(new SpinnerNumberModel(1, 1, 32, 1));
+		yearSpinner.setModel(new SpinnerNumberModel(2000, 1900, 3000, 1));
+		JPanel dobPanel = new JPanel();
+		dobPanel.add(monthSpinner);
+		dobPanel.add(daySpinner);
+		dobPanel.add(yearSpinner);
+
+		JLabel dobLabel = new JLabel("Next Visit Date:", SwingConstants.RIGHT);
 		String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 		monthSpinner.setModel(new SpinnerListModel(months));
 		daySpinner.setModel(new SpinnerNumberModel(1, 1, 32, 1));
